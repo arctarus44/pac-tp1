8f7071f746e759e126482c36b7372c2b0d89190f
arctarus44 <arthur.dewarumez@gmail.com>
2015-01-24 14:59:02 +0100

    Application des corrections envoyé par le prof, et ajout d'une fonction de chiffrement déchiffrement.

--- client.py
+++ client.py
@@ -37,11 +37,42 @@ def server_query( url, parameters=None ):
             data = json.dumps(parameters).encode(ENCODING)
             request.add_header('Content-type', 'application/json')
         with urllib.request.urlopen(request, data) as connexion:
+            result = connexion.read().decode(ENCODING)
             if connexion.info()['Content-Type'] == "application/json":
-                result = json.load(connexion)
-            else:
-                result = connexion.read().decode(ENCODING)
+                result = json.loads(result)
         return result
      except urllib.error.HTTPError as e:
         print('error while accessing {2} : [{0}] {1}'.format(e.code, e.reason, url))
         print('the server also says: ' + e.read().decode(ENCODING))
+
+
+import subprocess
+
+class DecryptionError(Exception):
+    pass
+
+def enc(msg, cipher="aes-128-cbc", passphrase=None, base64=True, decrypt=False):
+    """invoke the OpenSSL library (though the openssl executable which must be
+       present on your system to encrypt or decrypt content using a symmetric cipher.
+
+       # encryption use
+       >>> c = enc("toto", passphrase="foobar")
+
+       # decryption use
+       >>> enc(c, passphrase="foobar", decrypt=True)
+       'toto'
+       """
+
+    args = ["openssl", "enc", "-" + cipher]
+    if base64:
+        args.append("-base64")
+    if passphrase:
+        args.append("-k")
+        args.append(passphrase)
+    if decrypt:
+        args.append('-d')
+    result = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = result.communicate(msg.encode(ENCODING))
+    if stderr == "bad decrypt\n":
+        raise DecryptionError()
+    return stdout.decode(ENCODING)
