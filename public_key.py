c5e5197dff36fc22888dec7555b5acf1bd32364a
arctarus44 <arthur.dewarumez@gmail.com>
2015-02-02 18:16:27 +0100

    FIn script.

--- public_key.py
+++ public_key.py
@@ -1,4 +1,5 @@
 import client as c
+import os
 
 if __name__ == '__main__':
 	file = open("my_pkey", 'r')
@@ -9,3 +10,14 @@ if __name__ == '__main__':
 	file = open('./enc_resp', 'w')
 	file.write(resp)
 	file.close()
+	os.system('base64 -d enc_resp > out')
+	os.system('openssl rsautl -decrypt -in out -out unc_msg -inkey my_skey ')
+
+	file = open("./unc_msg", 'r')
+	end = {}
+
+	print(file.read())
+	print(c.BASE_URL);
+
+
+	# print(c.BASE_URL + c.server_query(, end))
